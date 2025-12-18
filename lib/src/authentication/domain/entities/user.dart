cb0244aa90e6e0d1f55a5eee3530a257a1d83223
raj-arthur-lima <120500238+raj-arthur-lima@users.noreply.github.com>
2025-12-18 14:00:40 -0300

    Desenv data layer
    Desenv data layer

--- lib/src/authentication/domain/entities/user.dart
+++ lib/src/authentication/domain/entities/user.dart
@@ -1,7 +1,7 @@
 import 'package:equatable/equatable.dart';
 
 class User extends Equatable {
-  final int id;
+  final String id;
   final String name;
   final String createdAt;
   final String avatar;
@@ -14,7 +14,7 @@ class User extends Equatable {
 
   const User.empty()
     : this(
-        id: 1,
+        id: '1',
         createdAt: '_empty.createdAt',
         name: '_empty.name',
         avatar: '_empty.avatar',
